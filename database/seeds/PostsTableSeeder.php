ee5714ec048cdc621374e35fbe9953ad59180154
SKILL573 <a9101021249@gmail.com>
2017-11-15 16:26:27 +0800

    練習四

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,18 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach(range(1,20)as$number){
+            Post::truncate();            
+            Post::create([
+            $faker=Faker::create('zh_TW');
+            'title'=>$faker‐>sentence,
+            'content'=>$faker‐>paragraph,
+            'is_feature'=>rand(0,1),
+            'created_at'=>Carbon::now()‐>subDays($total‐$number),
+            'updated_at'=>Carbon::now()‐>subDays($total‐$number),
+            ]);
+            }            
     }
+    $total=20;
+    
 }
